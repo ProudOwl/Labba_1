cbbbcc98814273b1093102dcf0a084f122f0fea9
ProudOwl <solovlad59@mail.ru>
2025-03-13 15:44:40 +0700

    Add file Laba_1.cpp

---
+++ Laba_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double perimeter(double top, double left){
+    return (top+left)*2;
+}
+
+double square(double top, double left){
+    return top*left;
+}
+
+double diagonal(double top, double left){
+    return sqrt(pow(top, 2) + pow(left, 2));
+}
+
+int main(){
+double top = 0, down, left = 0, right;
+while (top <= 0) {
+    cout << "Введите большую сторону прямоугольника ";
+    cin >> top;
+}
+down = top;
+while (left <= 0) {
+    cout << "Введите меньшую сторону прямоугольника ";
+    cin >> left;
+}
+right = left;
+cout << "Периметр = " << perimeter(top, left) << endl;
+cout << "Площадь = "<< square(top, left) << endl;
+cout << "Диагональ равна = " << diagonal(top, left) << endl;
+return 0;
+}
