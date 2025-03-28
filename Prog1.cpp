4c5666eb206f65a011e0798cc68e89ea9ef22b35
TempLogin <128985213+Oleg43414@users.noreply.github.com>
2025-03-28 13:22:31 +0700

    Add files via upload

--- Prog1.cpp
+++ Prog1.cpp
@@ -1,55 +1,56 @@
 #include <iostream>
 #include <cmath>
 
-double perimeter (double footing, double wall_left, double wall_right){
-	return (footing + wall_left + wall_right);
+double perimeter(double footing, double wall_left, double wall_right) {
+    return (footing + wall_left + wall_right);
 }
 
-double square (double footing, double wall_left, double wall_right){
-	return sqrt ((footing + wall_left + wall_right)/2 * ((footing + wall_left + wall_right)/2 - footing) * ((footing + wall_left + wall_right)/2 - wall_left) * ((footing + wall_left + wall_right)/2 - wall_right));
+double square(double footing, double wall_left, double wall_right) {
+    double s = (footing + wall_left + wall_right) / 2;
+    return sqrt(s * (s - footing) * (s - wall_left) * (s - wall_right));
 }
 
-double lsosceles (double footing, double wall_left, double wall_right){
-  if (footing == wall_left || footing == wall_right || wall_left==wall_right){
-                return 1;
-        }
-        else{
-                return 0;
-        }
+double lsosceles(double footing, double wall_left, double wall_right) {
+    return (footing == wall_left || footing == wall_right || wall_left == wall_right) ? 1 : 0;
 }
 
-int main()
-{
-        double footing = 0, wall_left = 0,  wall_right = 0; //основание, правая и левая сторона
+double inputSide(const std::string& prompt) {
+    double value;
+    while (true) {
+        std::cout << prompt;
+        std::cin >> value;
 
-        while(footing<=0){
-        std::cout<<" Введите основание треугольника" ;
-        std::cin>>footing;
+        // Проверка на некорректный ввод
+        if (std::cin.fail() || value <= 0) {
+            std::cout << "Некорректный ввод! Введите положительное число." << std::endl;
+            std::cin.clear(); // Сброс состояния потока
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Пропуск оставшегося ввода
+        } else {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Открытие остатка ввода
+            return value;
         }
+    }
+}
 
-        while(wall_left<=0){
-        std::cout<<" Введите левую сторону треугольника" ;
-        std::cin>>wall_left;
-        }
+int main() {
+    double footing = inputSide("Введите основание треугольника: ");
+    double wall_left = inputSide("Введите левую сторону треугольника: ");
+    double wall_right = inputSide("Введите правую сторону треугольника: ");
 
-        while(wall_right<=0){
-        std::cout<<" Введите правую сторону треугольника" ;
-        std::cin>>wall_right;
-	}
-
-	if (footing>=(wall_left+wall_right) || wall_left>=(footing+wall_right) || wall_right >= (footing+wall_left)){
-        std::cout<<"Такого треугольника не существует!!!"<<std::endl;
+    if (footing >= (wall_left + wall_right) || wall_left >= (footing + wall_right) || wall_right >= (footing + wall_left)) {
+        std::cout << "Такого треугольника не существует!!!" << std::endl;
         return 0;
+    }
+
+    std::cout << "Периметр = " << perimeter(footing, wall_left, wall_right) << std::endl;
+    std::cout << "Площадь = " << square(footing, wall_left, wall_right) << std::endl;
+    
+    if (lsosceles(footing, wall_left, wall_right) == 1) {
+        std::cout << "Треугольник равнобедренный" << std::endl;
+    } else {
+        std::cout << "Треугольник не равнобедренный" << std::endl;
+    }
+    
+    return 0;
 }
 
-
-        std::cout << "Периметр = " << perimeter(footing, wall_left, wall_right) << std::endl;
-        std::cout << "Площадь = " << square(footing, wall_left, wall_right) << std::endl;
-        double y = lsosceles(footing, wall_left, wall_right);
-        if (y==1){
-                std::cout<<"Треугольник равнобедренный"<<std::endl;
-        }
-        else{
-                std::cout<<"Треугольник не равнобедренный"<<std::endl;
-        }
-}       	
